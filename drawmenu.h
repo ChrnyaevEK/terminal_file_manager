126eb937130a35875debe8e779c8a3ca60f4ba12
ChrnyaevEK <36514797+ChrnyaevEK@users.noreply.github.com>
2020-11-14 18:20:32 +0100

    Add files via upload

---
+++ drawmenu.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+void DrawMenu(); //создание меню
+void gotoxy(int x, int y); // перевод курсора в точку x,y
+void itemMenu(int sel, bool activate); // выделить пункт меню
+void cls(int it = 0); // очистка консоли; при it==0 оставляем
+// строку меню иначе очищаем всю консоль
+void getCursorPosition(void); // запомнить положение курсора
+//в глобальную переменную curspos
+void showCursor(bool visible);// скрыть/показать курсор
